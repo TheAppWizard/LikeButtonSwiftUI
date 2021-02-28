1cd4bfb36b1aab8a8f630e14590a2555048202c3
Shreyas Bhike <70090469+TheAppWizard@users.noreply.github.com>
2021-02-28 23:47:11 +0530

    Add files via upload

---
+++ LikeButtonSwiftUI/LikeButtonSwiftUI/LikeButtonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikeButtonSwiftUIApp.swift
+//  LikeButtonSwiftUI
+//
+//  Created by Shreyas Vilaschandra Bhike on 28/02/21.
+//
+
+import SwiftUI
+
+@main
+struct LikeButtonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
